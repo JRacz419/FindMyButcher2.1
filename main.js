896db03204c1b94a35ee879ba29a166cc4738db8
Josh <jracz419@gmail.com>
2024-03-30 04:22:31 -0400

    Added business card section

--- main.js
+++ main.js
@@ -36,3 +36,21 @@ countries.forEach(country => {
         options.innerHTML = arr ? arr : '<p style="margin-top: 10px;">Oops! State not found</p>'; });  
         
         selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
+
+        /*cards*/
+
+
+const observer = new IntersectionObserver((entries) =>  {
+	entries.forEach ((entry) => {
+		console.log(entry)
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		}
+		else {
+			entry.target.classList.remove('show');
+		}
+	})
+})
+
+const hiddenElements = document.querySelectorAll('.card');
+hiddenElements.forEach((el) => observer.observe (el));
